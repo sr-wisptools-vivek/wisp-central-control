ca4ebb8698979eed47eabbd7e1da1ab2a05624d9
sirhanshafahath <sirhan@iozeta.com>
2015-03-09 21:40:06 +0530

    Fixed the select box value saving, and recognises plain array as options

--- packages/wisptools:autoform-select-other/autoform_select_other.js
+++ packages/wisptools:autoform-select-other/autoform_select_other.js
@@ -1,89 +1,63 @@
 AutoForm.addInputType("select-other", {
-  template: "afSelectOther",
-  valueOut: function () {
-    if (this.val() == 'other') {
-      return $("#"+this.attr('name')+"_other").val();
-    } else {
-      return this.val();
-    }
-  },
-  contextAdjust: function (context) {
-    if(typeof context.inputbox == "undefined") {
-      context.inputbox = context.name + '_other';
-    }
-    context.selectoptions = [];
-    $.each(context.selectOptions, function(i, e){
-      if (e.optgroup) {
-        $.each(e.options, function(j,f) {
-          if (context.value == f.value) {
-            _.extend(context.selectOptions[i].e.options[j],{selected: 'selected'});
-          }
-          context.selectoptions.push(f.value);
-        });
-      } else {
-        if (context.value == e.value) {
-          _.extend(context.selectOptions[i].e,{selected: 'selected'});
-        }
-        context.selectoptions.push(e.value);
-      }
-    });
-    if(_.indexOf(context.selectoptions, context.value) == -1) {
-      context.inputboxvalue = context.value;
-    } else {
-      context.inputboxvalue = '';
-    }
-    return context;
-  }
+	template: "afSelectOther",
+	valueOut: function () {
+		if (this.val() == 'other') {
+			return $("#"+this.attr('name')+"_other").val();
+		} else {
+			return this.val();
+		}
+	},
+	contextAdjust: function (context) {
+		if(typeof context.inputbox == "undefined") {
+			context.inputbox = context.name + '_other';
+		}
+		context.selectoptions = [];
+		$.each(context.selectOptions, function(i, e) {
+			if (e.optgroup) {
+				$.each(e.options, function(j, f) {
+					if (typeof f !== "object") {
+						context.selectOptions[i].options[j] = f = {label: f,value: f};
+					}
+					if (context.value!="" && context.value == f.value) {
+						_.extend(context.selectOptions[i].options[j],{selected: 'selected'});
+					}
+					context.selectoptions.push(f.value);
+				});
+			} else {
+				if (typeof e !== "object") {
+					context.selectOptions[i] = e = {label: e,value: e};
+				}
+				if (context.value !="" && context.value == e.value) {
+					_.extend(context.selectOptions[i],{selected: 'selected'});
+				}
+				context.selectoptions.push(e.value);
+			}
+		});
+		if(context.value!="" && _.indexOf(context.selectoptions, context.value) == -1) {
+			context.inputboxvalue = context.value;
+			context.selectedother = 'selected';
+		} else {
+			context.inputboxvalue = '';
+			context.selectedother = '';
+		}
+		return context;
+	}
 });
 
 Template.afSelectOther.helpers({
-  log: function() {
-    //console.log(this);
-  },
-  optionAtts: function () {
-    var item = this;
-    var atts = {
-      value: item.value
-    };
-    if (item.selected) {
-      atts.selected = '';
-    }
-    return atts;
-  },
-  atts: function () {
-    var atts = _.clone(this.atts);
-    atts = AutoForm.Utility.addClass(atts, 'form-control');
-    return atts;
-  }
+	optionAtts: function () {
+		var item = this;
+		var atts = {
+			value: item.value
+		};
+		if (item.selected) {
+			atts.selected = '';
+		}
+		return atts;
+	},
+	atts: function () {
+		var atts = _.clone(this.atts);
+		atts = AutoForm.Utility.addClass(atts, 'form-control');
+		return atts;
+	}
 });
-
-Template.afSelectOther.events({
-  'change select': function(event) {
-    if(event.currentTarget.value != 'other') {
-      //alert(event.currentTarget.name);
-      //alert($('#'+event.currentTarget.name+'_other').val());
-    }
-  }
-});
-
-Template.afSelectOther.rendered = function () {
-  context = this.data;
-  context.selectoptions = [];
-  $.each(context.selectOptions, function(i, e){
-    if (e.optgroup) {
-      $.each(e.options, function(j,f) {
-        if (context.value == f.value) {
-          _.extend(context.selectOptions[i].e.options[j],{selected: 'selected'});
-        }
-        context.selectoptions.push(f.value);
-      });
-    } else {
-      if (context.value == e.value) {
-        _.extend(context.selectOptions[i].e,{selected: 'selected'});
-      }
-      context.selectoptions.push(e.value);
-    }
-  });
-  //console.log(context.selectoptions);
-  
-};

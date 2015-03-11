079056b5e11ca0a088c4cee8760f0be5c91992ba
sirhanshafahath <sirhan@iozeta.com>
2015-03-11 23:34:28 +0530

    Fixed the other box selecting issue on empty input box. Fixed form autosave (JS file changed)

--- packages/wisptools:autoform-select-other/autoform_select_other.js
+++ packages/wisptools:autoform-select-other/autoform_select_other.js
@@ -2,7 +2,8 @@ AutoForm.addInputType("select-other", {
 	template: "afSelectOther",
 	valueOut: function () {
 		if (this.val() == 'other') {
-			return $("#"+this.attr('name')+"_other").val();
+			other = $(this).parent().next().find('input#'+this.attr('name')+'_other').val();
+			return (other == '') ? 'other' : other;
 		} else {
 			return this.val();
 		}
@@ -18,7 +19,7 @@ AutoForm.addInputType("select-other", {
 					if (typeof f !== "object") {
 						context.selectOptions[i].options[j] = f = {label: f,value: f};
 					}
-					if (context.value!="" && context.value == f.value) {
+					if (context.value == f.value) {
 						_.extend(context.selectOptions[i].options[j],{selected: 'selected'});
 					}
 					context.selectoptions.push(f.value);
@@ -27,18 +28,17 @@ AutoForm.addInputType("select-other", {
 				if (typeof e !== "object") {
 					context.selectOptions[i] = e = {label: e,value: e};
 				}
-				if (context.value !="" && context.value == e.value) {
+				if (context.value == e.value) {
 					_.extend(context.selectOptions[i],{selected: 'selected'});
 				}
 				context.selectoptions.push(e.value);
 			}
 		});
-		if(context.value!="" && _.indexOf(context.selectoptions, context.value) == -1) {
-			context.inputboxvalue = context.value;
-			context.selectedother = 'selected';
+		if(_.indexOf(context.selectoptions, context.value) == -1) {
+			if (context.value != 'other') context.inputboxvalue = context.value;
+			if (context.value != "") context.selectedother = 'selected';
 		} else {
 			context.inputboxvalue = '';
-			context.selectedother = '';
 		}
 		return context;
 	}
@@ -59,5 +59,13 @@ Template.afSelectOther.helpers({
 		var atts = _.clone(this.atts);
 		atts = AutoForm.Utility.addClass(atts, 'form-control');
 		return atts;
+	}
+});
+
+Template.afSelectOther.events({
+	'keyup input': function(event) {
+		formId = $(event.currentTarget).parent().parent().parent().parent().attr('id');
+		if ($("form#"+formId+" select[name='"+this.name+"']").val() == 'other')
+			$("form#"+formId+" select[name='"+this.name+"']").trigger('change');
 	}
 });

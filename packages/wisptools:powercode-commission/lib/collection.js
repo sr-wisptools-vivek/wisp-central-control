c7329d9c4b7a6e1617e477d494b798b841f96529
Sobhith <sobhith@wisptools.com>
2015-09-02 11:18:55 +0530

    added new collection report

--- packages/wisptools:powercode-commission/lib/collection.js
+++ packages/wisptools:powercode-commission/lib/collection.js
@@ -3,3 +3,4 @@ WtPowercodeCommission.collection = {};
 WtPowercodeCommission.collection.user = WtCollection('wt_powercode_commission_user');
 WtPowercodeCommission.collection.service = WtCollection('wt_powercode_commission_service');
 WtPowercodeCommission.collection.type = WtCollection('wt_powercode_commission_type');
+WtPowercodeCommission.collection.report=WtCollection('wt_powercode_commission_report');

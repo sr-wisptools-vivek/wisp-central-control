cc55a1353f5d5e87f82525dca9e494453957d4a9
Sobhith <sobhith@wisptools.com>
2015-09-10 13:36:40 +0530

    Added package file for roles

--- packages/wisptools:roles/package.js
+++ packages/wisptools:roles/package.js
@@ -12,7 +12,38 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('roles.js');
+  
+  
+   api.use([
+    'meteor',
+    'mongo',
+    'templating',
+    'reactive-var',
+    'underscore',
+    'iron:router@1.0.7',
+    'wisptools:tab-page',
+    'rajit:bootstrap3-datepicker@1.4.1',
+    'numeral:numeral@1.5.3',
+    'percolate:synced-cron@1.2.1',
+    'wisptools:growl',
+    'wisptools:collection',
+    'wisptools:menu'
+    
+  ]);
+  
+  api.addFiles([
+   
+    'lib/router.js'
+   
+    ], ['server','client']);
+    
+   api.addFiles([
+       'client/menu.js',
+    'client/templates/users.html',
+    'client/templates/users.js'
+    ], 'client');
+    
+    api.addFiles('roles.js');
 });
 
 Package.onTest(function(api) {

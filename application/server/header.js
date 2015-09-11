0091a3dd89577ed9e974e1e0e59690dd4ee970de
Sobhith <sobhith@wisptools.com>
2015-09-11 13:34:58 +0530

    added user roles

--- application/server/header.js
+++ application/server/header.js
@@ -3,16 +3,29 @@
 var users={roles:['admin']}
 }
 */
-
-Accounts.onCreateUser(function(options, user){
-      var users=[
-      {name:user.username,roles:['admin']},
-         ];
-         
-         _.each(users,function(userData)
-         {
-            username:userData.name;
-            
-         });
+Accounts.onCreateUser( function (options, user) {
+if(Meteor.users.find().count()<1)
+{
     
+user.roles = ['admin'];
+if ( options.profile )
+user.profile ={name:user.username};
+
+if (user.roles.length > 0) {
+Roles.addUsersToRoles(user._id,['admin']);
+}
+return user;
+}
+else
+{
+user.roles = ['Customer'];
+if ( options.profile )
+user.profile = {name:user.username};
+
+if (user.roles.length > 0) {
+Roles.addUsersToRoles(user._id,['Customer']);
+}
+return user;
+}
 });
+

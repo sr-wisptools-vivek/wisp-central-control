7e184b9fe7cbd2d13d03b9d1fd3faf404f802702
Sobhith <sobhith@wisptools.com>
2015-09-14 13:38:30 +0530

    Adding route for roles menu

--- packages/wisptools:roles/lib/router.js
+++ packages/wisptools:roles/lib/router.js
@@ -1,4 +1,18 @@
-Router.route('/admin/roles', {
-  name: 'rolesUser', 
-  template: 'wtRolesUser'
+Router.map(function() {
+  this.route('/admin/roles', {
+    path: '/admin/roles',
+    name: 'rolesUser', 
+    template: 'wtRolesUser',
+
+    onBeforeAction: function() {
+      user = Meteor.user();
+      if(!Roles.userIsInRole(user, ['admin'])) {
+       
+        return;
+      }
+   
+     this.next();
+    }
+  })
 });
+

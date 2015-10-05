c51e74d9ac91b54bd313421a5027e6a419b9e4e4
Spencer Lambert <spence@wisptools.com>
2015-10-05 16:46:43 -0600

    fixing code

--- packages/wisptools:roles/client/templates/users.js
+++ packages/wisptools:roles/client/templates/users.js
@@ -1,32 +1,26 @@
 
-   
-
 Template.wtRolesUser.created = function(){
-var self = this;
-Meteor.subscribe('admin');
+  var self = this;
+  Meteor.subscribe('admin');
   self.userList = new ReactiveVar([]);
-var data =Meteor.users.find();
-//console.log(data);
-var count = Meteor.users.find().count();
- self.userList.set(data);
-
+  var data =Meteor.users.find();
+  var count = Meteor.users.find().count();
+  self.userList.set(data);
 }
 
 Template.wtRolesUser.helpers({
-   userList: function(){ 
-       return Template.instance().userList.get();
-   }
+  userList: function(){ 
+    return Template.instance().userList.get();
+  }
 });
 
 Template.wtRolesUsersSelect.created = function(){
-    var sel=this;
-    sel.typeList= new ReactiveVar([]);
-
-    
+  var sel=this;
+  sel.typeList= new ReactiveVar([]);
 }
 
 Template.wtRolesUsersSelect.helpers({
- userType: function () {
+  userType: function () {
      
     var User = Template.parentData(1);
     var _id=User._id
@@ -46,7 +40,7 @@ Template.wtRolesUsersSelect.helpers({
     return data;
   },
   typeList: function () {
-      Meteor.subscribe('roles');
+    Meteor.subscribe('roles');
     return Meteor.roles.find();  
   },
   selected: function (a, b) {
@@ -55,39 +49,28 @@ Template.wtRolesUsersSelect.helpers({
 });
 
 Template.wtRolesUsersSelect.events({
-    "change .type-sel": function (event) {
-        var User = Template.parentData(0);
-     
-      var name;
-        var roles=event.target.value;
-       if(User.emails)
-          name=User.emails[0].address;
-       else if(User.services.google.email)
-          name=User.services.google.email;
-      
-        var data = {
-          id: User._id,
-          roles: event.target.value,
-          name:name
-        
-        }
-        var user=Meteor.user();
-        if(user._id==User._id)
-        {
-             WtGrowl.fail("Sorry!! you cannot update your own role ");
-         }
-         else
-         {
-        Meteor.call('updateRoles',data.id,data.roles,function (err, res) {
-         if (err)
-            WtGrowl.fail("Could not update Role type for user " + data.name);
-         else
-         {
-            WtGrowl.success("Role type updated for user " + data.name);
-         }
-        
-        });
-         }
+  "change .type-sel": function (event) {
+    var User = Template.parentData(0);
+    var name;
+    var roles=event.target.value;
+
+    var data = {
+      id: User._id,
+      roles: event.target.value
     }
+
+    var user=Meteor.user();
+    if(user._id==User._id) {
+      WtGrowl.fail("Sorry!! you cannot update your own role ");
+    } else {
+      Meteor.call('updateRoles',data.id,data.roles,function (err, res) {
+        if (err)
+          WtGrowl.fail("Could not update Role type for user " + data.name);
+        else {
+          WtGrowl.success("Role type updated for user " + data.name);
+        }
+      });
+    }
+  }
 });
 

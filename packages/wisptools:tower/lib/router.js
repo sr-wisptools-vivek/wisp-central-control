f65cd68ede45ba64076093579479aea9ea1cb5b0
Spencer Lambert <spence@digis.net>
2015-12-02 19:53:26 -0700

    Removing router route.

--- packages/wisptools:tower/lib/router.js
+++ packages/wisptools:tower/lib/router.js
@@ -10,9 +10,3 @@ Router.onBeforeAction(function() {
   GoogleMaps.load({libraries: 'geometry'});
   this.next();
 }, { only: ['towerMap'] });
-
-Router.route('/routers', {
-  name: 'managedRouter',
-  template: 'managedRouter',
-});
-

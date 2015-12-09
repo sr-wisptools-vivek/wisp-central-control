6abd2491e695cc57e47d66903e6a4f2f82b0bafc
abhijithsj <test.abhijith@gmail.com>
2015-12-09 06:30:44 -0500

    added friendly-tech API functions and meteor mmethods

--- packages/wisptools:friendly-tech-api/server/functions.js
+++ packages/wisptools:friendly-tech-api/server/functions.js
@@ -1,8 +1,8 @@
 
-WtFriendlyTech.FTGetDeviceInfo = function(username, fullname, telephone) {
+WtFriendlyTech.FTGetDeviceInfo = function(deviceSerialNo) {
   
   var url = Meteor.settings.friendlytech.url;
-  var args = {'username': username, 'fullname': fullname, 'telephone': telephone};
+  var args = {devicesn: deviceSerialNo};
 
   try
   {
@@ -10,6 +10,136 @@ WtFriendlyTech.FTGetDeviceInfo = function(username, fullname, telephone) {
     //console.log(Soap);
     var client = Soap.createClient(url);
     console.log(client);
+    var result = client.FTGetDeviceInfo(args);
+    console.log(result);
+    return result;
+  }
+  catch (err)
+  {
+    if(err.error === 'soap-creation')
+    {
+      console.log('SOAP Client creation failed');
+    }
+    else if (err.error === 'soap-method')
+    {
+      console.log('SOAP Method call failed');
+    }
+    return err;
+  }
+}
+
+WtFriendlyTech.FTCPEStatus = function(deviceSerialNo) {
+  
+  var url = Meteor.settings.friendlytech.url;
+  var args = {devicesn: deviceSerialNo};
+
+  try
+  {
+    var client = Soap.createClient(url);
+    var result = client.FTCPEStatus(args);
+    console.log(result);
+    return result;
+  }
+  catch (err)
+  {
+    if(err.error === 'soap-creation')
+    {
+      console.log('SOAP Client creation failed');
+    }
+    else if (err.error === 'soap-method')
+    {
+      console.log('SOAP Method call failed');
+    }
+    return err;
+  }
+}
+
+WtFriendlyTech.FTRebootDevice = function(deviceSerialNo) {
+  
+  var url = Meteor.settings.friendlytech.url;
+  var args = {devicesn: deviceSerialNo};
+
+  try
+  {
+    var client = Soap.createClient(url);
+    var result = client.FTRebootDevice(args);
+    console.log(result);
+    return result;
+  }
+  catch (err)
+  {
+    if(err.error === 'soap-creation')
+    {
+      console.log('SOAP Client creation failed');
+    }
+    else if (err.error === 'soap-method')
+    {
+      console.log('SOAP Method call failed');
+    }
+    return err;
+  }
+}
+
+WtFriendlyTech.FTResetToDefault = function(deviceSerialNo) {
+  
+  var url = Meteor.settings.friendlytech.url;
+  var args = {devicesn: deviceSerialNo};
+
+  try
+  {
+    var client = Soap.createClient(url);
+    var result = client.FTResetToDefault(args);
+    console.log(result);
+    return result;
+  }
+  catch (err)
+  {
+    if(err.error === 'soap-creation')
+    {
+      console.log('SOAP Client creation failed');
+    }
+    else if (err.error === 'soap-method')
+    {
+      console.log('SOAP Method call failed');
+    }
+    return err;
+  }
+}
+
+WtFriendlyTech.FTGetDeviceParameters = function(deviceSerialNo) {
+
+  var url = Meteor.settings.friendlytech.url;
+  var CPEStatus = function(deviceSerialNo){
+    var APIArgs = {devicesn:deviceSerialNo};
+    var client = Soap.createClient(url);
+    var result = client.FTCPEStatus(APIArgs);
+    console.log(result);
+    if (result.FTCPEStatusResult.ErrorCode == 100)
+      {
+        var responseData = result.FTCPEStatusResult;
+        if(responseData.Online == true)
+        {
+          return "0";
+        }
+        else
+        {
+          return "1";
+        }
+      }
+    else
+      {
+        return "1";
+      }
+  };
+
+  var args = {devicesn: deviceSerialNo, 
+              arraynames:{string: "InternetGatewayDevice.ManagementServer.URL", string: "InternetGatewayDevice.Time.NTPServer1"},
+              source: CPEStatus(deviceSerialNo)
+            };
+console.log(args);
+  try
+  {
+    var client = Soap.createClient(url);
     var result = client.FTGetDeviceParameters(args);
     console.log(result);
     return result;
--- packages/wisptools:managed-router/managed-router.js
+++ packages/wisptools:managed-router/managed-router.js
@@ -2,10 +2,75 @@
 if (Meteor.isServer) {
 	Meteor.methods({
 
-		  "wtManagedRouterGetInfo": function(username, fullname, telephone){
+		  "wtManagedRouterGetInfo": function(deviceSerialNo){
 	      //console.log(username);
-	      var response = WtFriendlyTech.FTGetDeviceInfo(username, fullname, telephone);
-	      console.log(response);
+	      var response = WtFriendlyTech.FTGetDeviceInfo(deviceSerialNo);
+	      //console.log(response);
+	      if (response.FTGetDeviceInfoResult.ErrorCode == 100)
+	      	{
+	      		var responseData = response.FTGetDeviceInfoResult;
+	      		return "ManufacturerName : "+responseData.ManufacturerName+" ModelName : "+responseData.ModelName;
+	      	}
+	      else
+	      	{
+	      		return "failed";
+	      	}
+    	},
+    	"wtManagedRouterGetCPE": function(deviceSerialNo){
+	      //console.log(username);
+	      var response = WtFriendlyTech.FTCPEStatus(deviceSerialNo);
+	      //console.log(response);
+	      if (response.FTCPEStatusResult.ErrorCode == 100)
+	      	{
+	      		var responseData = response.FTCPEStatusResult;
+	      		return "Online : "+responseData.Online;
+	      	}
+	      else
+	      	{
+	      		return "failed";
+	      	}
+    	},
+    	"wtManagedRouterRebootDevice": function(deviceSerialNo){
+	      //console.log(username);
+	      var response = WtFriendlyTech.FTRebootDevice(deviceSerialNo);
+	      //console.log(response);
+	      if (response.FTRebootDeviceResult.ErrorCode == 100)
+	      	{
+	      		var responseData = response.FTRebootDeviceResult;
+	      		return "Id : "+responseData.Id;
+	      	}
+	      else
+	      	{
+	      		return "failed";
+	      	}
+    	},
+    	"wtManagedRouterResetToDefault": function(deviceSerialNo){
+	      //console.log(username);
+	      var response = WtFriendlyTech.FTResetToDefault(deviceSerialNo);
+	      //console.log(response);
+	      if (response.FTResetToDefaultResult.ErrorCode == 100)
+	      	{
+	      		var responseData = response.FTResetToDefaultResult;
+	      		return "Id : "+responseData.Id;
+	      	}
+	      else
+	      	{
+	      		return "failed";
+	      	}
+    	},
+    	"wtManagedGetDeviceParameters": function(deviceSerialNo){
+	      //console.log(username);
+	      var response = WtFriendlyTech.FTGetDeviceParameters(deviceSerialNo);
+	      //console.log(response);
+	      if (response.FTGetDeviceParametersResult.ErrorCode == 100)
+	      	{
+	      		var responseData = response.FTGetDeviceParametersResult;
+	      		return "Params : "+responseData.Params.ParamWSDL.Value;
+	      	}
+	      else
+	      	{
+	      		return "failed";
+	      	}
     	}
 
 	});

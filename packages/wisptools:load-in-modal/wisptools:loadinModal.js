88e47850ceb511f17fcf4cab57bf7a601cead99b
bevin-pavithran <bevinpavithran@gmail.com>
2016-04-08 12:15:18 +0530

    View Router is now using Iframe to load the content

--- packages/wisptools:load-in-modal/wisptools:loadinModal.js
+++ packages/wisptools:load-in-modal/wisptools:loadinModal.js
@@ -1,10 +1,10 @@
 (function ($) {
 
-	$.fn.loadInModal = function () {
+	$.fn.loadInModal = function (childSelector) {
 
 		createModal();
 
-		this.on('click', function (e) {
+		this.on('click', childSelector, function (e) {
 			e.preventDefault();
 			showpopup(e.target.href);
 		});
@@ -14,14 +14,16 @@
 	};
 
 	function showpopup(url) {
+		var iframe = '<iframe style="width:100%;height:500px;border:none;" id="iframeinmodal"></iframe>';
+		$('#iframeinmodal').remove();
+		$('#modalIframe').append(iframe);
 		$('#iframeinmodal').attr('src', url);
 		$('#modalforiframe').modal('show');
 	}
 
 	function createModal() {
-		var modal = '<div class="modal fade" id="modalforiframe" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-body"><iframe style="width:100%;height:500px;border:none;" id="iframeinmodal"></iframe></div></div></div></div></div>';
-
-$('body').append(modal);
+		var modal = '<div class="modal fade" id="modalforiframe" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div id="modalIframe" class="modal-body"></div></div></div></div></div>';
+		$('body').append(modal);
 	}
 
 })(jQuery);

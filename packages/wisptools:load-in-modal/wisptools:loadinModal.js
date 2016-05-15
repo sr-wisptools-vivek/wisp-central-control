1291f438ed8ce8649cf920e90dae8352055236b7
Spencer Lambert <spence@wisptools.com>
2016-05-15 17:53:15 -0600

    adjusting the style of the modal

--- packages/wisptools:load-in-modal/wisptools:loadinModal.js
+++ packages/wisptools:load-in-modal/wisptools:loadinModal.js
@@ -25,7 +25,7 @@
 	}
 
 	function createModal() {
-		var modal = '<div class="modal fade" id="modalforiframe" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h6 class="modal-title" id="myModalLabel">&nbsp;</h4></div><div id="modalIframe" class="modal-body"><div id="siteloader-content"><h3>Fetching Data From Device</h3><img id="loader" src="/img/siteloader.gif"/></div></div></div></div></div></div>';
+		var modal = '<div class="modal fade modal-container" id="modalforiframe" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h6 class="modal-title" id="myModalLabel">&nbsp;</h4></div><div id="modalIframe" class="modal-body"><div id="siteloader-content"><h3>Fetching Data From Device</h3><img id="loader" src="/img/siteloader.gif"/></div></div></div></div></div></div>';
 		$('body').append(modal);
 		var heightOfModal = Math.floor($(document).height() * 0.8);
 		$('#modalIframe').css('height',heightOfModal+'px');

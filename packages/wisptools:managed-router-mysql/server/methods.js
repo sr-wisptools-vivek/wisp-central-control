84068b0b007cb13dff1f752608b508cfd5e07cf0
Spencer Lambert <spence@wisptools.com>
2016-08-07 19:35:49 -0600

    adding backend slave events

--- packages/wisptools:managed-router-mysql/server/methods.js
+++ packages/wisptools:managed-router-mysql/server/methods.js
@@ -276,6 +276,24 @@ Meteor.method("wtManagedRouterMySQLAdd", function(router) {
   runQuery(sql, fut);
   res = fut.wait();
 
+  // Backend Event - Add Equipment
+  fut = new Future();
+  sql = 
+    "INSERT INTO " +
+    " " + db_name + ".Changes " +
+    "VALUES ( " +
+    " null, " +
+    " 1, " +
+    " 1, " +
+    " 'AddUpdateEquipment', " +
+    " '', " +
+    " " + eId + ", " +
+    " null, " +
+    " '0000-00-00 00:00:00' " +
+    ")";
+  runQuery(sql, fut);
+  res = fut.wait();
+
   return search.call(this, router.serial, 1);
 },{
   url: "/mr/add"
@@ -406,6 +424,24 @@ Meteor.method("wtManagedRouterMySQLRemove", function(router){
         "status" : "Deleted"
   };
 
+  // Backend Event - Delete Equipment
+  fut = new Future();
+  sql = 
+    "INSERT INTO " +
+    " " + db_name + ".Changes " +
+    "VALUES ( " +
+    " null, " +
+    " 1, " +
+    " 1, " +
+    " 'DeleteEquipment', " +
+    " '', " +
+    " " + equipmentId + ", " +
+    " null, " +
+    " '0000-00-00 00:00:00' " +
+    ")";
+  runQuery(sql, fut);
+  res = fut.wait();
+
   return response;
 
 },{
@@ -451,6 +487,24 @@ Meteor.method("wtManagedRouterMySQLRestore", function(router){
   runQuery(sql,fut);
   var res = fut.wait();
   
+  // Backend Event - Add/Update Equipment
+  fut = new Future();
+  sql = 
+    "INSERT INTO " +
+    " " + db_name + ".Changes " +
+    "VALUES ( " +
+    " null, " +
+    " 1, " +
+    " 1, " +
+    " 'AddUpdateEquipment', " +
+    " '', " +
+    " " + equipmentId + ", " +
+    " null, " +
+    " '0000-00-00 00:00:00' " +
+    ")";
+  runQuery(sql, fut);
+  res = fut.wait();  
+
   return search.call(this, router.serial, 1);
 },{
   url: "/mr/undelete"

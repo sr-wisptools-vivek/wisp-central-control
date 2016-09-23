c1e753868ede1cd1d0a78ab5ac10d009209273c2
bevin-pavithran <bevinpavithran@gmail.com>
2016-09-23 16:50:46 +0530

    Update user page to edit domain name. Code to update domain name is not working, adding domain to new users is working

--- packages/wisptools:managed-router-mysql/client/domains.js
+++ packages/wisptools:managed-router-mysql/client/domains.js
@@ -4,28 +4,44 @@ Template.wtManagedRouterMySQLDomains.helpers({
   }
 });
 
-Template.wtManagedRouterMySQLDomain.helpers({
+Template.wtManagedRouterMySQLDomain.helpers({    
   domainName: function (userId) {
     var domain = WtMangedRouterMySQLDomains.findOne({userId: userId});
     if (domain) {
       return domain.name;
     }
     return "";
+  },
+  domains: function () {
+    return WtMangedRouterMySQLDomainsList.find();
+  },  
+  selected: function (userId) {
+      var domain = this.domain;
+      var userDomain = WtMangedRouterMySQLDomains.findOne({userId: userId});
+      if(userDomain){
+        return (domain == userDomain.name) ? 'selected' : '';
+      }
+      return '';
   }
 });
 
 Template.wtManagedRouterMySQLDomain.events({
-  "blur .mr-domain": function (event, template) {
-    var userId = template.data._id;
+  "change .mr-domain-select": function () {
+    var userId = this.user;
+    var newDomain = $('.mr-domain-select').val();
     var domain = WtMangedRouterMySQLDomains.findOne({userId: userId});
-    var name = $(event.target).val();
-
+    var test;
+    console.log(domain);
     if (domain) {
-      WtMangedRouterMySQLDomains.update({_id: domain._id}, {$set: {name: name}});
+      WtMangedRouterMySQLDomains.update({_id: domain._id}, {$set: {name: newDomain}});
+      //testing Domain name not being updated. 
+      test = WtMangedRouterMySQLDomains.findOne({_id: domain._id});
+      console.log(test);
+      console.log(domain._id);
     } else {
       WtMangedRouterMySQLDomains.insert({
         userId: userId,
-        name: name
+        name: newDomain
       });
     }
     WtGrowl.success('Domain updated.');

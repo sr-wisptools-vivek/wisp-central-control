7c20e64dbf0bd29f4a377fa59aafa02ec5b546dd
Spencer Lambert <spence@wisptools.com>
2016-10-08 14:15:56 -0600

    changing Username text to Email for login and sign up

--- application/client/config.js
+++ application/client/config.js
@@ -1,3 +1,14 @@
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
-})
+});
+
+// forcing username field to say Email...
+// need to figure out how to convert to real email login.
+accountsUIBootstrap3.map('en', {
+    signupFields: {
+      username: "Email"
+    },
+    loginFields: {
+      username: "Email"
+    }
+});

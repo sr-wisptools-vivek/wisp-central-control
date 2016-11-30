422d38cd9c2d6000b10d63598a60b3bf9249a496
Vivek Dinesh <vivek@wisptools.com>
2016-11-30 17:58:09 +0530

    Work on adding account registration domain field

--- application/client/config.js
+++ application/client/config.js
@@ -1,3 +1,23 @@
 Accounts.ui.config({
   passwordSignupFields: 'EMAIL_AND_LEGACY_USERNAME'
 });
+
+Accounts.ui.config({
+  extraSignupFields: [{
+    fieldName: 'domain',
+    fieldLabel: 'Domain',
+    inputType: 'text',
+    visible: true,
+    validate: function(value, errorFunction) {
+      if (!value || (value && value.trim().length<1)) {
+        errorFunction("Please provide a valid domain.");
+        return false;
+      } else if (value.trim().indexOf(" ") > -1) {
+        errorFunction("Domain name should not contain spaces.");
+      } else {
+
+        return true;
+      }
+    }
+  }]
+});
--- packages/wisptools:managed-router-domains/server/methods.js
+++ packages/wisptools:managed-router-domains/server/methods.js
@@ -4,8 +4,21 @@ Meteor.method("wtManagedRouterDeleteDomain", function(){
   var item = WtMangedRouterMySQLDomains.findOne({name: removeDomain.domain});
   if (item) {
     Session.set('managedRouterDomainDelete', null);
-    throw new Meteor.Error('denied','Domain Assigned to User');    
+    throw new Meteor.Error('denied','Domain Assigned to User');
   } else {
     WtMangedRouterMySQLDomainsList.remove({_id: removeDomain._id});
+  }
+});
+
+Meteor.method("wtManagedRouterCheckDomain", function (domain) {
+  if (domain && domain.trim().length>0 && domain.trim().indexOf(" ")==-1) {
+    var domainList = WtMangedRouterMySQLDomainsList.findOne({domain: domain});
+    if (!domainList) {
+      throw new Meteor.Error('error','Domain name is not valid.');
+    } else {
+      return true;
+    }
+  } else {
+    throw new Meteor.Error('error','Domain name is not valid.');
   }
 });

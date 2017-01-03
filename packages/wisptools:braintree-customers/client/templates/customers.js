4d946bb4f5fb4f8f7fa5b444fb1a33c4b856d40f
Vivek Dinesh <vivek@wisptools.com>
2017-01-03 18:00:44 +0530

    Search Braintree customers

--- packages/wisptools:braintree-customers/client/templates/customers.js
+++ packages/wisptools:braintree-customers/client/templates/customers.js
@@ -1,5 +1,6 @@
 Template.wtBraintreeCustomers.onRendered(function () {
   Session.set('braintreeCustomers', false);
+  Session.set('braintreeCustomersSearchFilter', false);
   Meteor.call('wtBraintreeCustomerGetCustomers', 10, function (e, r) {
     if (!e) {
       Session.set('braintreeCustomers', r);
@@ -9,6 +10,45 @@ Template.wtBraintreeCustomers.onRendered(function () {
 
 Template.wtBraintreeCustomers.helpers({
   'customers': function () {
-    return Session.get('braintreeCustomers');;
+    return Session.get('braintreeCustomers');
+  },
+  'hasSearchFilter': function () {
+    var filter = Session.get('braintreeCustomersSearchFilter');
+    if (filter===false) {
+      return false;
+    }
+    return true;
+  },
+  'getSearchFilter': function () {
+    return Session.get('braintreeCustomersSearchFilter');
+  }
+});
+
+Template.wtBraintreeCustomers.events({
+  'click .addCustomerBtn': function (e) {
+    e.preventDefault();
+    Router.go('wtBraintreeCustomersAdd');
+  },
+  'click .searchBtn': function (e) {
+    e.preventDefault();
+    var query = $('#searchBox').val();
+    Session.set('braintreeCustomersSearchFilter', query);
+    Session.set('braintreeCustomers', false);
+    Meteor.call('wtBraintreeCustomerGetCustomers', 10, query, function (e, r) {
+      if (!e) {
+        Session.set('braintreeCustomers', r);
+      }
+    });
+  },
+  'click .clearFilter': function (e) {
+    e.preventDefault();
+    $('#searchBox').val('');
+    Session.set('braintreeCustomersSearchFilter', false);
+    Session.set('braintreeCustomers', false);
+    Meteor.call('wtBraintreeCustomerGetCustomers', 10, function (e, r) {
+      if (!e) {
+        Session.set('braintreeCustomers', r);
+      }
+    });
   }
 });
--- packages/wisptools:braintree-customers/server/methods.js
+++ packages/wisptools:braintree-customers/server/methods.js
@@ -17,13 +17,34 @@ Meteor.methods({
       owner: this.userId
     });
   },
-  'wtBraintreeCustomerGetCustomers': function (count) {
+  'wtBraintreeCustomerGetCustomers': function (count, query) {
     if (!this.userId) throw new Meteor.Error(401, "Not authorized");
     if (!Roles.userIsInRole(this.userId, ['domain-admin'])) throw new Meteor.Error(401, "Not authorized");
 
-    return WtBraintreeCustomers.collection.find(
-      {owner: this.userId, domain: Meteor.user().profile.domain},
-      {sort: {createdAt: -1}, limit: count}
-    ).fetch();
+    if (!count) {
+      count = 10;
+    }
+
+    if (!query || (query && query.length==0)) {
+      return WtBraintreeCustomers.collection.find(
+        {owner: this.userId, domain: Meteor.user().profile.domain},
+        {sort: {createdAt: -1}, limit: count}
+      ).fetch();
+    } else {
+      var limitQuery = {limit: count};
+      var re = new RegExp(query, 'i');
+      var crit = {
+        owner: this.userId,
+        domain: Meteor.user().profile.domain,
+        $or: [
+          {'customerId': re},
+          {'firstName': re},
+          {'lastName': re},
+          {'phone': re},
+          {'email': re}
+        ]
+      };
+      return WtBraintreeCustomers.collection.find(crit, limitQuery).fetch();
+    }
   }
 });

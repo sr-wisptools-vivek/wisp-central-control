1d15326dfe4ef4ed857f0c6611b91ab6a2b9f3e9
Spencer Lambert <spence@wisptools.com>
2017-02-10 12:37:24 -0700

    adding shipped button and report details

--- packages/wisptools:free-router/client/details.js
+++ packages/wisptools:free-router/client/details.js
@@ -2,4 +2,19 @@ Template.wtFreeRouterDetails.helpers({
   item: function() {
     return WtFreeRouter.findOne({ _id: Template.instance().data.itemId });
   },
+  notEquals: function (a, b) {
+    return a !== b;
+  },
+  yesOrNo: function (test) {
+    return test ? "Yes" : "No";
+  }
 });
+
+Template.wtFreeRouterDetails.events({
+  'click .mark-shipped': function(e, t) {
+    e.preventDefault();
+    e.stopPropagation();
+    WtFreeRouter.update({'_id':t.data.itemId},{$set:{'status':'Shipped'}});
+    WtGrowl.success('Record Updated');
+  }
+})

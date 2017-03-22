8e53a161dedad80ae7604a9bdcb91562f29d7548
Spencer Lambert <spence@wisptools.com>
2017-03-21 21:00:02 -0600

    adding instructions on using braintree

--- packages/wisptools:braintree-customers/client/menu.js
+++ packages/wisptools:braintree-customers/client/menu.js
@@ -1,3 +1,3 @@
-WtMenu.addDropdown('Customer', 'fa-sitemap', 0, ['domain-admin', 'customer']);
+WtMenu.addDropdown('Customer', 'fa-user', 0, ['domain-admin', 'customer']);
 WtMenu.addDropdownItem('Customer', 'Add', 'wtBraintreeCustomersAdd', 'fa-plus', 1, ['domain-admin', 'customer']);
-WtMenu.addDropdownItem('Customer', 'List', 'wtBraintreeCustomers', 'fa-list', 1, ['domain-admin', 'customer']);
+WtMenu.addDropdownItem('Customer', 'List', 'wtBraintreeCustomers', 'fa-list', 2, ['domain-admin', 'customer']);
--- packages/wisptools:braintree/client/menu.js
+++ packages/wisptools:braintree/client/menu.js
@@ -1,3 +1,3 @@
-WtMenu.addDropdown('Domain Admin', 'fa-sitemap', 0, ['domain-admin']);
-WtMenu.addDropdownItem('Domain Admin', 'Braintree Settings', 'wtBraintreeSettings', 'fa-cog', 1, ['domain-admin']);
-WtMenu.addDropdownItem('Domain Admin', 'Braintree Plans', 'wtBraintreePlans', 'fa-list', 1, ['domain-admin']);
+//WtMenu.addDropdown('Domain Admin', 'fa-sitemap', 0, ['domain-admin']); // Set in Domain Invite Package
+WtMenu.addDropdownItem('Domain Admin', 'Braintree Settings', 'wtBraintreeSettings', 'fa-cog', 2, ['domain-admin']);
+WtMenu.addDropdownItem('Domain Admin', 'Braintree Plans', 'wtBraintreePlans', 'fa-usd', 3, ['domain-admin']);

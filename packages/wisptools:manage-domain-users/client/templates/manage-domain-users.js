3661648b319f363bb5b08ab5742ecfad905138f1
Vivek Dinesh <vivek@wisptools.com>
2017-07-12 16:21:22 +0530

    Enable or disable domain users

--- packages/wisptools:manage-domain-users/client/templates/manage-domain-users.js
+++ packages/wisptools:manage-domain-users/client/templates/manage-domain-users.js
@@ -4,7 +4,7 @@ Template.wtManageDomainUsers.onCreated(function () {
 
 Template.wtManageDomainUsers.onRendered(function () {
   var _this = this;
-  Meteor.call('wtManageUserDomainsGetDomainUsers', function (err, res) {
+  Meteor.call('wtManageDomainUsersGetDomainUsers', function (err, res) {
     if (!err && res && res.length>0) {
       _this.domainUsers.set(res);
     }
@@ -20,5 +20,58 @@ Template.wtManageDomainUsers.helpers({
       return this.emails[0].address;
     }
     return '';
+  },
+  'isEnabled': function () {
+    if (this.profile && this.profile.disabled===true) {
+      return false;
+    }
+    return true;
+  },
+  'isCurrentUser': function () {
+    if (this._id === Meteor.userId()) {
+      return true;
+    }
+    return false;
+  }
+});
+
+Template.wtManageDomainUsers.events({
+  'click .disableBtn': function () {
+    var domainUsers = Template.instance().domainUsers;
+    Meteor.call('wtManageDomainUsersDisableUser', this._id, function (e, r) {
+      if (e) {
+        WtGrowl.fail(e.message);
+      } else {
+        if (!r) {
+          WtGrowl.fail('Failed to disable user.');
+        } else {
+          Meteor.call('wtManageDomainUsersGetDomainUsers', function (err, res) {
+            if (!err && res && res.length>0) {
+              domainUsers.set(res);
+              WtGrowl.success('Disabled user.');
+            }
+          });
+        }
+      }
+    });
+  },
+  'click .enableBtn': function () {
+    var domainUsers = Template.instance().domainUsers;
+    Meteor.call('wtManageDomainUsersEnableUser', this._id, function (e, r) {
+      if (e) {
+        WtGrowl.fail(e.message);
+      } else {
+        if (!r) {
+          WtGrowl.fail('Failed to disable user.');
+        } else {
+          Meteor.call('wtManageDomainUsersGetDomainUsers', function (err, res) {
+            if (!err && res && res.length>0) {
+              domainUsers.set(res);
+              WtGrowl.success('Enabled user.');
+            }
+          });
+        }
+      }
+    });
   }
 });
--- packages/wisptools:manage-domain-users/server/methods.js
+++ packages/wisptools:manage-domain-users/server/methods.js
@@ -1,4 +1,4 @@
-Meteor.method('wtManageUserDomainsGetDomainUsers', function () {
+Meteor.method('wtManageDomainUsersGetDomainUsers', function () {
   if (!this.userId) throw new Meteor.Error('denied','Not Authorized');
   if (!Roles.userIsInRole(this.userId, ['domain-admin'])) throw new Meteor.Error(401, "Not authorized");
 
@@ -13,6 +13,40 @@ Meteor.method('wtManageUserDomainsGetDomainUsers', function () {
       if (domainUserIds && domainUserIds.length>0) {
         return Meteor.users.find({_id: {$in: domainUserIds}}).fetch();
       }
+    }
+  }
+  return false;
+});
+
+Meteor.method('wtManageDomainUsersDisableUser', function (userId) {
+  if (!this.userId) throw new Meteor.Error('denied','Not Authorized');
+  if (!Roles.userIsInRole(this.userId, ['domain-admin'])) throw new Meteor.Error(401, "Not authorized");
+
+  var domain = WtMangedRouterMySQLDomains.findOne({userId: this.userId});
+  if (domain && domain.name) {
+    var domainUser = WtMangedRouterMySQLDomains.findOne({userId: userId, name: domain.name});
+    if (!domainUser) {
+      throw new Meteor.Error("User not found or you are not authorized to modify this user.");
+    } else {
+      Meteor.users.update({_id: userId}, {$set: {"profile.disabled": true}});
+      return true;
+    }
+  }
+  return false;
+});
+
+Meteor.method('wtManageDomainUsersEnableUser', function (userId) {
+  if (!this.userId) throw new Meteor.Error('denied','Not Authorized');
+  if (!Roles.userIsInRole(this.userId, ['domain-admin'])) throw new Meteor.Error(401, "Not authorized");
+
+  var domain = WtMangedRouterMySQLDomains.findOne({userId: this.userId});
+  if (domain && domain.name) {
+    var domainUser = WtMangedRouterMySQLDomains.findOne({userId: userId, name: domain.name});
+    if (!domainUser) {
+      throw new Meteor.Error("User not found or you are not authorized to modify this user.");
+    } else {
+      Meteor.users.update({_id: userId}, {$set: {"profile.disabled": false}});
+      return true;
     }
   }
   return false;
